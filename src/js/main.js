32060a57a7c69cb312c7f2613d62527b202ed3ae
burikAV <AVburik84@gmail.com>
2019-10-02 16:38:53 +0300

    стилизация модального окна

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var modal = document.getElementsByClassName('modal');
+var btn = document.getElementById('btn-about');
+var close = document.getElementsByClassName('close')[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+close.onclick = function() {
+  modal.style.display = 'none';
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
